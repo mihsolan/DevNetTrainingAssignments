6ecb326fa98a795391a238d1288ae70abe122cd7
mihsolan <64087674+mihsolan@users.noreply.github.com>
2020-06-01 21:19:26 +0530

    improved tester and finished json

--- scripts/UnitTester.py
+++ scripts/UnitTester.py
@@ -2,14 +2,19 @@ import unittest
 
 class test_data(unittest.TestCase):
     def test_data_children(self,namelist):
-        actuallist=["ACCT400","ACCT500","ACCT600"]
-        self.assertEqual(actuallist,namelist)
+        self.count=0
+        for names in namelist:
+            self.assertEqual("ACCT",names[0:4])
+            self.count+=1
     
     def test_data_paid(self,paidlist):
-        actuallist=["600","70","0"]
-        self.assertEqual(actuallist,paidlist)
+        self.assertEqual(self.count,len(paidlist))
+        for data in paidlist:
+            self.assertEqual(float,type(float(data)))
+        
     
     def test_data_due(self,duelist):
-        actuallist=["10000","40","0"]
-        self.assertEqual(actuallist,duelist)
+        self.assertEqual(self.count,len(duelist))
+        for data in duelist:
+            self.assertEqual(float,type(float(data)))
     
